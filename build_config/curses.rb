6738fe99e85e01da53a7ab1bb026c0fc3aa3bf22
masahino <masahino@gmail.com>
2024-01-13 07:59:13 +0900

    add include paths

--- build_config/curses.rb
+++ build_config/curses.rb
@@ -13,4 +13,10 @@ MRuby::Build.new('mrbmacs-curses') do |conf|
   gem_config(conf)
 
   conf.gem github: 'masahino/mruby-bin-mrbmacs-curses'
+  if RUBY_PLATFORM.downcase =~ /msys|mingw/
+    conf.cc.include_paths << "#{MRUBY_ROOT}/../misc"
+    conf.cc.include_paths << "#{ENV['MINGW_PREFIX']}/include/pdcurses"
+    conf.cc.flags << '-DPDC_WIDE'
+    conf.cc.flags << '-DPDC_FORCE_UTF8'
+  end
 end
