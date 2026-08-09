c1fa82cf3c8ef7a0af4d4f66a8605ace55ba1c7b
masahino <masahino@gmail.com>
2026-08-09 12:46:05 +0900

    Link mruby core archive in all builds

--- build_config/common.rb
+++ build_config/common.rb
@@ -1,3 +1,11 @@
+class MRuby::Build
+  # mruby 3.3+ splits core objects into libmruby_core.a, but gem binaries
+  # in this project still expect both archives to be linked.
+  def libraries
+    [libmruby_static, libmruby_core_static]
+  end
+end
+
 def gem_config(conf)
   conf.cc.defines << %w[MRB_UTF8_STRING]
 
